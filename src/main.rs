b4680ab946f4cc504e312f8b45f33bf86faba351
ajg7 <ajgebara@gmail.com>
2021-02-12 11:15:23 -0600

    set up cargo and tested to make sure that it runs

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+/*
+Rules:
+    1) Start as one block
+    2) Eat food, and gain one block
+    3) Hit the edge, then game over (restart)
+
+*/
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
